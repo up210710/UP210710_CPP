100a7426784dfa43191285df39f729590d9740c6
up210710 <up210710@alumnos.upa.edu.mx>
2022-09-13 13:06:42 -0500

    Probando comunicacion

--- U1/Hola.cpp
+++ U1/Hola.cpp
@@ -1,1 +1,14 @@
-getchar();
+/*
+#include<iostream>
+#include<conio.h>
+#include<stdio.h>
+using namespace std; 
+int main () 
+{
+char pausa;
+cout<<"Hola mundo";
+pausa=getchar();
+return 0;
+}
+*/
+
---
+++ U1/parimpar.cpp
@@ -0,0 +1,8 @@
+/*
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Par o impar"<<endl<<"\n"<<endl;
+    return 0;
+}
+*/
