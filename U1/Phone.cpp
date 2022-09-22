03dba1413a474f052f86711d301fe6b7db1e0412
up210710 <up210710@alumnos.upa.edu.mx>
2022-09-22 12:33:19 -0500

    Prueba_A

---
+++ U1/Phone.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+char NOTA;
+int main()
+{
+    cout<<"Teclee la nota";
+    cin>>NOTA;
+    switch(NOTA)
+    {
+        case 'A':
+        cout<<"sobresaliente";
+        break;
+        case 'B':
+        cout<<"Notable";
+        break;
+        case 'C':
+        cout<<"Aprobado";
+        break;
+        case 'D':
+        cout<<"Reprobado";
+        break;
+        default:
+        cout<<"Nota no valida";
+        }
+    return 0;
+}
