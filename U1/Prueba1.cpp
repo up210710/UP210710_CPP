f94732669ba052f0a5466643a514ac6b1181465a
up210710 <up210710@alumnos.upa.edu.mx>
2022-09-22 12:34:10 -0500

    Prueba_B

---
+++ U1/Prueba1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+float SUMA=0;
+int N,i=1;
+int main()
+{
+    cout<<"Teclee hasta que numero desea la suma:";
+    cin>>N;
+    while(i<=N)
+    {
+        SUMA=SUMA+(i/(pow(2,i)));
+        i=i+1;
+        }
+        cout<<"La suma es:" <<SUMA;
+    return 0; 
+}
