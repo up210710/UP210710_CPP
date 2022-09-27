38322eda4d1db612e2c205235ea7c452adb7cefa
up210710 <up210710@alumnos.upa.edu.mx>
2022-09-27 12:29:41 -0500

    Correcion

--- U1/Ejercicio_Edad.cpp
+++ U1/Ejercicio_Edad.cpp
@@ -1,33 +1,38 @@
 #include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     int E;
-    cout<<"Introduce la edad de una familiar: ";
-    cin>>E;
-    cout<<endl;
-    if(E>0 && E<=150){
-        if(E<=30){
-            cout<<"Primera edad ";
-            cout<<endl;
+    cout << "Introduce la edad de una familiar: ";
+    cin >> E;
+    cout << endl;
+    if (E > 0 && E <= 150)
+    {
+        if (E <= 30)
+        {
+            cout << "Primera edad ";
+            cout << endl;
         }
-        else{
-        if(E>=31 && E<=60){
-            cout<<"Segunda edad ";
-            cout<<endl;
+        else if (E >= 31 && E <= 60)
+        {
+            cout << "Segunda edad ";
+            cout << endl;
         }
-        if(E>=61 && E<=90){
-            cout<<"Tercera edad ";
-            cout<<endl;
+        else if (E >= 61 && E <= 90)
+        {
+            cout << "Tercera edad ";
+            cout << endl;
         }
-        if(E>=91 && E<=150){
-            cout<<"Horas extras ";
-            cout<<endl;
-        }
+        else if (E >= 91 && E <= 150)
+        {
+            cout << "Horas extras ";
+            cout << endl;
         }
     }
-    else{
-        cout<<"Bajale carnal ";
-        cout<<endl;
+    else
+    {
+        cout << "Bajale carnal ";
+        cout << endl;
     }
     getchar();
     return 0;
