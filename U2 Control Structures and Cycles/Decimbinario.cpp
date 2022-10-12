18f92fd5a53165679972918098318fa0a59583f7
up210710 <up210710@alumnos.upa.edu.mx>
2022-10-12 16:48:10 -0500

    Polo

---
+++ U2 Control Structures and Cycles/Decimbinario.cpp
@@ -0,0 +1,58 @@
+/*
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    string cadena;
+
+    cadena=cadena+"U";
+    cadena=cadena+"P";
+    cadena=cadena+"2";
+    cadena=cadena+"1";
+    cadena=cadena+"0";
+    cadena=cadena+"7";
+    cadena=cadena+"1";
+    cadena=cadena+"0";
+    cout << cadena;
+    return 0;
+}
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    string cadena;
+    
+	cadena=cadena+"U";
+    cout<<cadena<<endl;
+    cadena=cadena+"P";
+    cout<<cadena<<endl;
+    cadena=cadena+"2";
+    cout<<cadena<<endl;
+    cadena=cadena+"1";
+    cout<<cadena<<endl;
+    cadena=cadena+"0";
+    cout<<cadena<<endl;
+    cadena=cadena+"7";
+    cout<<cadena<<endl;
+    cadena=cadena+"1";
+    cout<<cadena<<endl;
+    cadena=cadena+"0";
+    cout<<cadena<<endl;
+    return 0;
+}
+*/
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int n=7;
+    int r;
+    float q;
+    r=n/2;
+    q=n/2;
+    return 0;
+}
